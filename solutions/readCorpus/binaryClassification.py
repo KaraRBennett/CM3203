2b1b1ad6ff5840f67967cd1e14f75b3db5205625
Kara Bennett <22744773+KaraRBennett@users.noreply.github.com>
2020-06-02 18:59:27 +0100

    Corrected ID slicing for label files

--- solutions/readCorpus/binaryClassification.py
+++ solutions/readCorpus/binaryClassification.py
@@ -11,19 +11,22 @@ def readText(directory, fileExtenstion = '.txt'):
     for i, file in enumerate(corpusFiles):
         file = open(file, encoding='utf-8')
         for line in file:
-            articleID.append(corpusFiles[i][articleStringIndex:-(len(fileExtenstion))])
+            articleID.append(corpusFiles[i][articleStringIndex:-len(fileExtenstion)])
             text.append(line[:-1])
     return { 'articleID' : articleID, 'text' : text }
 
 
-def readLabels(directory, fileExtenstion = '.labels'):
+def readLabels(directory, fileExtenstion = '.task-SLC.labels'):
     articleID = []
     label = []
-    labelFiles = getFiles(directory, fileExtenstion)
-    for i, file in enumerate(labelFiles):
+    corpusFiles = getFiles(directory, fileExtenstion)
+
+    articleStringIndex = corpusFiles[0].rfind('article') + 7
+
+    for i, file in enumerate(corpusFiles):
         file = open(file)
         for line in file:
             line = line.split('\t')
-            articleID.append(labelFiles[i][:-len(fileExtenstion)])
+            articleID.append(corpusFiles[i][articleStringIndex:-len(fileExtenstion)])
             label.append(line[-1][:-1])
     return { 'articleID' : articleID, 'label' : label}
