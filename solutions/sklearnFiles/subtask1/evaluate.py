1df5d4be2236b30839c8ac3ba20530b1f0e92e1f
Kara Bennett <22744773+KaraRBennett@users.noreply.github.com>
2020-06-02 09:52:19 +0100

    Changed evaluation output to be consistent with 3 tab format for subtask1

--- solutions/sklearnFiles/subtask1/evaluate.py
+++ solutions/sklearnFiles/subtask1/evaluate.py
@@ -1,18 +1,30 @@
 from translateLabels.translateBCLabels import Int2Word
 
-def unseenData(model, vectoriser, data, outputFile='evaluation.txt'):
+def unseenData(model, vectoriser, data, outputFile='Evaluation.txt'):
     output = []
 
     vectorisedData = vectoriser.transform(data['text'])
 
+    currentArticleID = data['articleID'][0]
+    sentenceNo = 0
+
     for i in range(len(data['text'])):
-        outputString = '{0}\t'.format(data['articleID'][i])
-        result = model.predict(vectorisedData[i])
-        outputString += Int2Word(result) + "\n"
+
+        if data['articleID'][i] == currentArticleID:
+            sentenceNo += 1
+        else:
+            currentArticleID = data['articleID'][i]
+            sentenceNo = 1
+        
+        outputString = '{0}\t{1}\t{2}\n'.format(
+            currentArticleID,
+            sentenceNo,
+            Int2Word(model.predict(vectorisedData[i]))
+        )
         output.append(outputString)
     
     print(len(output))
     
-    file = open(outputFile, 'w')
+    file = open('results/subtask1/sklearn-' + outputFile, 'w')
     file.writelines(output)
     file.close()
